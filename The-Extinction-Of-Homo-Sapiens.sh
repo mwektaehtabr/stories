b9715b18de7c1a0e93eb3a2137ad99c022a2a7d2
mwektaehtabr <mwektaehtabr@gmail.com>
2023-06-26 17:33:21 -0400

    //

---
+++ The-Extinction-Of-Homo-Sapiens.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# THE EXTINCTION OF HOMO SAPIENS
+# by mwektaehtabr
+# distributed under a Creative Commons CC0 1.0 Universal Public Domain Dedication
+
+statement="My number one priority in life is"
+extension="that my number one priority in life is"
+priorities=("myself." "my family." "my friends." "my work." "my religion." "my politics." "my country." "the planet.")
+priority=0
+
+stty -echo
+for number in {0..7}; do
+ clear
+done
+printf "\e[?25l"
+echo "THE EXTINCTION OF HOMO SAPIENS"
+echo
+echo -n $statement" "${priorities[$priority]}
+sleep 4
+while :; do
+ printf "\e[${#priorities[$priority]}D"
+ printf "\e[0J"
+ if [ $priority -eq ${#priorities} ]; then
+  priority=0
+  echo -n $extension" "
+ else
+  priority=$(($priority+1))
+ fi
+ echo -n ${priorities[$priority]}
+ sleep 4
+done
---
+++ Writers-Block.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# WRITER'S BLOCK
+# by mwektaehtabr
+# distributed under a Creative Commons CC0 1.0 Universal Public Domain Dedication
+
+for number in {0..7}; do
+ clear
+done
+printf "\e[?25l"
+echo "WRITER'S BLOCK"
+echo
+sleep 4
+while :; do
+ read writer
+ echo
+done
